34f72f7ce53144a65a6e9a304c1b73a3ee5bc1d1
zzeongho <junghoo022@gmail.com>
2023-03-16 17:41:22 +0900

    upgrade

--- main.js
+++ main.js
@@ -3,3 +3,33 @@ const tekitSchoolDiv = document.getElementById("tekit-school"); // get the Tekit
 tekitSchoolDiv.addEventListener("click", () => {
   window.location.href = "https://techit.education/school"; // set the URL of the Naver page you want to redirect to
 });
+
+const startupDiv = document.getElementById("startup"); // get the Tekit School div element
+
+startupDiv.addEventListener("click", () => {
+  window.location.href = "https://techit.education/school/kdt-startup-6th"; // set the URL of the Naver page you want to redirect to
+});
+
+const vodDiv = document.getElementById("vod"); // get the Tekit School div element
+
+vodDiv.addEventListener("click", () => {
+  window.location.href = "https://techit.education/classes"; // set the URL of the Naver page you want to redirect to
+});
+
+const educationDiv = document.getElementById("education"); // get the Tekit School div element
+
+educationDiv.addEventListener("click", () => {
+  window.location.href = "https://techit.education/enterprise"; // set the URL of the Naver page you want to redirect to
+});
+
+const eventDiv = document.getElementById("event"); // get the Tekit School div element
+
+eventDiv.addEventListener("click", () => {
+  window.location.href = "https://techit.education/event"; // set the URL of the Naver page you want to redirect to
+});
+
+const logo = document.querySelector(".techit-logo"); // get the Tekit School div element
+
+logo.addEventListener("click", () => {
+  window.location.href = "https://techit.education/"; // set the URL of the Naver page you want to redirect to
+});
