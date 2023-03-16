d7e81bf5a31967f13f219e8eb1fa5da8aaf4edf3
zzeongho <junghoo022@gmail.com>
2023-03-16 16:06:25 +0900

    second commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const tekitSchoolDiv = document.getElementById("tekit-school"); // get the Tekit School div element
+
+tekitSchoolDiv.addEventListener("click", () => {
+  window.location.href = "https://techit.education/school"; // set the URL of the Naver page you want to redirect to
+});
